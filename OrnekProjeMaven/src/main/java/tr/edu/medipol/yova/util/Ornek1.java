5e1643aef3fd96f5fed437375c29d2a3c48cbacb
Emircan <emircandv787@gmail.com>
2023-06-08 20:16:51 +0300

    first commit

---
+++ OrnekProjeMaven/src/main/java/tr/edu/medipol/yova/util/Ornek1.java
@@ -0,0 +1,17 @@
+package tr.edu.medipol.yova.util;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Ornek1 {
+
+	public static void main(String[] args) {
+	
+		String ornekGirdi = "  ab  c  ";
+		
+		String sonuc = StringUtils.deleteWhitespace(ornekGirdi);
+
+		System.out.println(sonuc);
+		
+	}
+
+}
